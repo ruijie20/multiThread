97021549f2370b4e5c8b8e782b78c43f32eed7b6
Ruijie Huang <ruijiehuang@Ruijies-mac.lan>
2017-09-12 22:07:24 +0800

    thread group uncaughtException

---
+++ src/Section12/MyThreadGroup.java
@@ -0,0 +1,15 @@
+package Section12;
+
+public class MyThreadGroup extends ThreadGroup {
+    public MyThreadGroup(String name) {
+        super(name);
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.printf("System has throw an exception %s\n", t.getId());
+        e.printStackTrace(System.out);
+        System.out.printf("Terminate the rest of the threads\n");
+        interrupt();
+    }
+}
