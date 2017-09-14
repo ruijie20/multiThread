95f382336eedda297bb1c444479d18912998b205
ruijiehuang <ruijieh20@gmail.com>
2017-09-15 02:54:08 +0800

    use conditions in lock

---
+++ src/Chapter2/Section8/Consumer.java
@@ -0,0 +1,27 @@
+package Chapter2.Section8;
+
+import java.util.Random;
+
+public class Consumer implements Runnable{
+    private Buffer buffer;
+
+    public Consumer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        while(buffer.hasPendingLines()){
+            String line = buffer.get();
+            processLine(line);
+        }
+    }
+
+    private void processLine(String line) {
+        try {
+            Thread.sleep(new Random().nextInt(100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
