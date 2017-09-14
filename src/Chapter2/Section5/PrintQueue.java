0e5d7977ee0ddbe13f7825037a0336930171e7cb
ruijiehuang <ruijieh20@gmail.com>
2017-09-14 16:16:47 +0800

    when print doucment lock this part

---
+++ src/Chapter2/Section5/PrintQueue.java
@@ -0,0 +1,23 @@
+package Chapter2.Section5;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PrintQueue {
+    private final Lock queueLock = new ReentrantLock();
+
+    public void printJob(Object document){
+        queueLock.lock();
+
+        try {
+            Long duration = (long)(Math.random() * 10000);
+            System.out.printf("%s : PrintQueue: printing a job during %d seconds\n",
+                    Thread.currentThread().getName(), duration/1000);
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+    }
+}
